d37d7f204b84ef0bd713a34394439e6ea1de253b
naveen-droid <59038779+naveen-droid@users.noreply.github.com>
2020-08-06 21:36:44 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,17 @@
+#oving a file
+#chnage the location of a file
+# new directory formation
+
+import os
+import shutil
+#os.mkdir("E:\\india_brain\\Day1\\temp")
+#shutil.move("E:\\india_brain\\Day1\\temp","E:\\india_brain")
+#shutil.copy("")
+
+#copy multiple files
+#for evry ifile,copy it.
+
+"""file_list=["E:\\india_brain\\original.pdf","E:\\india_brain\\Prims.pdf","E:\\india_brain\\dll.pdf"]
+for file in file_list:
+    os.remove(file)"""
+os.remove("E:\india_brain\Day1\Manipulating_files_and_folders\exm.txt")
